1e7998594ff984efffac259b7401fb86f2f58171
Sushant Bhaiswar <96516472+SushantBhaiswar@users.noreply.github.com>
2023-01-04 15:53:32 +0530

    Delete Backend directory

--- Backend/router.js/Routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const productcontroller = require("../controllers/productcontroller");
-
-router.get("/getproduct", productcontroller.getallproduct);
-
-module.exports = router;
